9234e2241eb6288b7031c7dbedcb1bd8789b2186
arshdeep <arshdeep@1mg-lptp-1117.local>
2020-05-13 18:12:39 +0530

    Updated Header.test.js

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Header from '../components/Header'
 import renderer from 'react-test-renderer'
+import {render, fireEvent, screen, cleanup} from '@testing-library/react'
 
+afterEach(cleanup)
 const classHeader = "sample class"
 const SampleData = new Set(["1st Sample",
     "2nd Sample",
@@ -22,3 +24,13 @@ it("Renders the Header correctly with test props", () => {
         .toJSON();
     expect(tree).toMatchSnapshot();
 })
+afterEach(cleanup)
+//functional testing
+it('Renders the correct results after the search keyword is entered', () => {
+render (<Header InitialData={SampleData}/>)
+    
+    fireEvent.change(screen.getByPlaceholderText('Type here to search',
+    { target: { value: "1st Sample" } }))
+    fireEvent.click(screen.getByText(/search/i))
+    expect(screen.getAllByText('1st Sample').length).toBe(2)
+});
